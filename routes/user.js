9c64b7ddc77a5d6bddf4fa50a3a9293b8d261c0f
laurenlittle <littlelauren016@gmail.com>
2019-11-29 21:40:49 -0500

    update user route and create test controller method

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const {
+  sayHi
+} = require('../controllers/user');
+
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
-});
+router.get('/', sayHi);
 
 module.exports = router;
