52e0e32cede72d32276f9e504517bfa340fab994
laurenlittle <littlelauren016@gmail.com>
2019-11-29 23:34:25 -0500

    correctly import user and remove salt/hashed_password from newUser

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,19 +1,22 @@
-const User = require('../models/user-document-models');
+const { User } = require('../models/user-document-models');
 
 exports.signup = (req, res) => {
   console.log('req.body', req.body);
 
-  const newUser = new User(req.body);
+  const newUser = new User(req.body); // complaining here - 
 
   newUser.save((err, user) => {
     if(err) {
       return res.status(400).json({
-        err
-      })
-    } else {
-      res.json({
-        user
+        error: err
       })
     }
+
+    newUser.salt = undefined;
+    newUser.hashed_password = undefined;
+
+    res.json({
+      user
+    })
   })
 };
